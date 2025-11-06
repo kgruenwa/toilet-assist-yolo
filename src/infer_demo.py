ee5a91abb47146e9b449b01f57a8fd560bd17e7b
KatharinaGruenwald <kgruenwa@hm.edu>
2025-11-06 13:51:35 +0100

    Some changes

--- src/infer_demo.py
+++ src/infer_demo.py
@@ -1,10 +1,13 @@
 from ultralytics import YOLO
 
-# Modell laden
-model = YOLO("yolo11s.pt")
+# Dein trainiertes Modell laden
+m = YOLO("runs/detect/train10/weights/best.pt")
+print(m.names)
+print(m.ckpt_path)  
+
 
 # Testbild auswerten
-results = model("docs/sample.jpg")
+results = m("docs/WC_5.11.2025.jpg")
 
 # Ergebnisse anzeigen
 results[0].show()
